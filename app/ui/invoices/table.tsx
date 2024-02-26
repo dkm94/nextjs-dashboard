bd1ee1eef2afdb9c78b180d2c3a774a21ecd93e1
dkm94 <diane.mpacko@gmail.com>
2024-02-26 03:19:18 +0100

    + chapter 11

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -12,6 +12,7 @@ export default async function InvoicesTable({
   currentPage: number;
 }) {
   const invoices = await fetchFilteredInvoices(query, currentPage);
+  console.log("🚀 ~ invoices 2:", invoices)
 
   return (
     <div className="mt-6 flow-root">
